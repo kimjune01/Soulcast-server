33d62fbfaf11b5f934ed0ffc0ae480398c25a21d
kimjune01 <kimjune01@gmail.com>
2015-02-24 15:35:55 -0800

    tried cleaning git

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-# app/assets/javascripts/application.js
-//= require video
+//= require_tree
+//= require videos
